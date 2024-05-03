43b2bf21f3ec8c74d683747c6923a4d92dccb751
Ming Yang <vivym@live.com>
2024-05-03 20:45:37 +0800

    RoleLlama3-8B SFT
    Signed-off-by: Ming Yang <vivym@live.com>

--- examples/full_multi_gpu/single_node_llama3_8b_v1.sh
+++ examples/full_multi_gpu/single_node_llama3_8b_v1.sh
@@ -1,35 +1,34 @@
 #!/bin/bash
 
-deepspeed --num_gpus 4 ../../src/train_bash.py \
+deepspeed --num_gpus 8 ../../src/train_bash.py \
     --deepspeed ../deepspeed/ds_z3_config.json \
     --stage sft \
     --do_train \
     --model_name_or_path meta-llama/Meta-Llama-3-8B \
-    --dataset alpaca_gpt4_en,glaive_toolcall \
+    --dataset roleplay-sft-v1 \
     --dataset_dir ../../data \
     --template llama3 \
     --finetuning_type full \
-    --output_dir ../../saves/Meta-Llama-3-8B/full/sft \
-    --overwrite_cache \
+    --output_dir ../../saves/RoleLlama3-8B/full_sft/v1 \
     --overwrite_output_dir \
     --cutoff_len 2048 \
     --preprocessing_num_workers 16 \
-    --per_device_train_batch_size 1 \
-    --per_device_eval_batch_size 1 \
-    --gradient_accumulation_steps 2 \
+    --per_device_train_batch_size 8 \
+    --per_device_eval_batch_size 8 \
+    --gradient_accumulation_steps 4 \
     --lr_scheduler_type cosine \
     --logging_steps 10 \
-    --warmup_steps 20 \
-    --save_steps 100 \
-    --eval_steps 100 \
+    --warmup_steps 100 \
+    --save_steps 200 \
+    --eval_steps 200 \
     --evaluation_strategy steps \
-    --learning_rate 5e-5 \
+    --learning_rate 1e-5 \
     --num_train_epochs 3.0 \
-    --max_samples 3000 \
-    --val_size 0.1 \
+    --max_samples 537204 \
+    --val_size 0.05 \
     --ddp_timeout 180000000 \
     --plot_loss \
     --bf16 \
     --flash_attn "fa2" \
     --report_to wandb \
-    --packing
+    --packing True
